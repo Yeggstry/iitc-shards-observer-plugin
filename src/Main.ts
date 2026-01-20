948edf6049ddf6c5cd75783f34ba0abadfa70f71
Lewis Keen <yeggstry@gmail.com>
2026-01-20 01:53:56 +0000

    feat: implement initial shards-core library logic

--- src/Main.ts
+++ src/Main.ts
@@ -1,5 +1,6 @@
 import * as Plugin from "iitcpluginkit";
 import { ShardsObserverUI } from "./ShardsObserverUi";
+import { Coordinates, haversineDistance } from "@ingress-shards/shards-core";
 
 class Shards_Observer implements Plugin.Class {
     private ui: ShardsObserverUI;
@@ -13,6 +14,19 @@ class Shards_Observer implements Plugin.Class {
             label: "Shards Observer",
             action: () => this.ui.show(),
         });
+
+        const coords1: Coordinates = {
+            latitude: 38.707008,
+            longitude: -9.13564,
+        };
+
+        const coords2: Coordinates = {
+            latitude: 35.223789,
+            longitude: -80.841141,
+        };
+
+        const distance = haversineDistance(coords1, coords2);
+        console.log(`Calculated distance: ${distance / 1000} km`);
     }
 }
 
